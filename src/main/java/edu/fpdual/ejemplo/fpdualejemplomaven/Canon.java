b309912d5ba7de0a7330f78c364f387cec211a1d
fcjdm <fcjdom@gmail.com>
2022-04-08 13:40:27 +0200

    Maven example

---
+++ src/main/java/edu/fpdual/ejemplo/fpdualejemplomaven/Canon.java
@@ -0,0 +1,10 @@
+package edu.fpdual.ejemplo.fpdualejemplomaven;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Canon {
+        private String letra;
+        private int frecuencia;
+}
